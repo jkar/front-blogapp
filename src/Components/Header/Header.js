6a5f26d4b9473b1dc4e280f46efaef9ad43c3a18
jkar <gianniskaramitsos3@gmail.com>
2021-02-04 23:08:03 +0200

    css fixed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,15 +9,6 @@ const Header = ({ blogs }) => {
                 {blogs.map((el, index) => {
                     return <Link className="link" key={el.id} to={`/blog/${index}`}>{el.name}</Link>
                 })}
-
-                {/* { user === null ? null : <p>{user.data.name} is logged in</p>}
-                { user === null ? null : <button onClick={logOut}>Log Out</button> } */}
-                {/* <Link to="/">Main </Link>
-                <Link to="/about">About Us </Link>
-                <Link to="/login">Log in </Link>
-                <Link to="/signup">Sign up</Link> */}
-                {/* { user !== null ? <h1 className="navbar"><Link to="/formProduct">Create Product</Link></h1> : null}
-                { user !== null ? <h1 className="navbar"><Link to="/editeDeleteProduct">Edit/Delete Product</Link></h1> : null} */}
             </nav>
     )
 };
